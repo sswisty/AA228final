8410fc921133853968381d3c025b48dfd145370b
Shawn Swist <sswisty7@gmail.com>
2018-12-05 11:44:37 -0800

    Messed that up.. here is the original working final project script

--- FinalProject.jl
+++ FinalProject.jl
@@ -2,7 +2,6 @@
 AA 228 Final Project
 Justin, Andrew, Shawn
 Stanford University, Fall 2018
-
 NOTES
 I have been setting this up like we talked about and using
 https://github.com/JuliaPOMDP/POMDPExamples.jl/blob/master/notebooks/Defining-a-POMDP-with-the-Generative-Interface.ipynb
@@ -14,14 +13,13 @@ using POMDPs
 using POMDPModels               # necessary?
 using POMDPSimulators           # necessary?
 using DifferentialEquations
-using Random
 include("OrbitMechFcns.jl")
 
 # Defining the POMDP
 struct SatPOMDP <: POMDP{Float64,Float64,Float64} # The three inputs are for state, action, observation, all floats
     r_close::Float64 # = -100      # 1/r^2 reward coefficient
     r_burn::Float64 # = -10        # burn reward, multiple for multiple directions?
-    discount::Float64  # .9              # Discount factor, necessary?
+    γ::Float64  # .9              # Discount factor, necessary?
     μ::Float64  # 398600.441      #
     Rₑ::Float64  # 6378.137       # [km] Earth radius
     J₂::Float64  # 1.0826267e-3   # Oblatness
@@ -30,7 +28,7 @@ end
 
 satPOMDP = SatPOMDP(-100,-10,.9,398600.441,6378.137,1.0826267e-3,1)
 
-function POMDPs.generate_sor(P::SatPOMDP, s, a, rng::AbstractRNG) # s,a are arrays...
+function POMDPs.generate_sor(P::SatPOMDP, s, a) # s,a are arrays...
     # Define the constants for the propagator
     tspan = (0,P.dt)
     params = [P.μ,P.Rₑ,P.J₂]
@@ -44,7 +42,7 @@ function POMDPs.generate_sor(P::SatPOMDP, s, a, rng::AbstractRNG) # s,a are arra
     sol2 = DifferentialEquations.solve(prob2,saveat = P.dt)
 
     # Build the next state vector
-    sp = [sol1.u[end]; sol2.u[end]] # transpose keeps sp a row vector...
+    sp = [sol1.u[end]' sol2.u[end]'] # transpose keeps sp a row vector...
 
     # Define the reward
     r = norm(sp[1:3]-sp[7:9])
@@ -57,7 +55,7 @@ function POMDPs.generate_sor(P::SatPOMDP, s, a, rng::AbstractRNG) # s,a are arra
     o = sp      # for testing...
 
 
-    return sp,o,R
+    return sp,R,o
 end
 
 
@@ -77,69 +75,12 @@ Rs,Vs = OE2ECI(oe_sat)
 a = Rₑ+500          # 500 km altitude orbit
 e = 0.0             #  circular orbit
 i = deg2rad(90)     #  polar orbit
-Ω = deg2rad(90)
+Ω = deg2rad(0)
 ω = 0
-ν = deg2rad(0.1)
+ν = 0
 oe_deb = OrbitalElementVec(a,e,i,Ω,ω,ν)
 Rd,Vd = OE2ECI(oe_deb)
 
-s = [Rs; Vs; Rd; Vd] # forcing this a row vector. Should we use a column? Julia defaults to columns
+s = [Rs' Vs' Rd' Vd'] # forcing this a row vector. Should we use a column? Julia defaults to columns
 
-test_s,test_o,test_r = POMDPs.generate_sor(satPOMDP,s,0.0,Random.seed!(1))
-
-
-using POMDPSimulators
-using POMDPPolicies
-POMDPs.initialstate_distribution(p::SatPOMDP) = s
-# policy = FunctionPolicy(o->0.0)
-planner = POMDPs.solve(solver, satPOMDP)
-
-for (s, a, r) in stepthrough(satPOMDP, planner, "s,a,r", max_steps=10)
-    @show s
-    @show a
-    @show r
-    println()
-end
-
-
-
-using POMCPOW
-using POMDPModels
-using POMDPSimulators
-using POMDPPolicies
-POMDPs.initialstate_distribution(p::SatPOMDP) = s
-POMDPs.discount(p::SatPOMDP) = 1
-POMDPs.action(p::SatPOMDP) = [-2 -1 0 1 2]
-solver = POMCPOWSolver(criterion=MaxUCB(20.0))
-planner = POMDPs.solve(solver, satPOMDP)
-# hr = HistoryRecorder(max_steps=100)
-# hist = simulate(hr, satPOMDP, planner)
-# for (s, b, a, r, sp, o) in hist
-#     @show s, a, r, sp
-end
-
-#=
-# Test outside of the loop so I can see what is going on
-tspan = (0,satPOMDP.dt)
-params = [satPOMDP.μ,satPOMDP.Rₑ,satPOMDP.J₂]
-
-# Set up as 2 solutions - sat and debris
-IC1 = s[1:6]  # Position and Velocity of satellite
-IC2 = s[7:12] # Position and Velocity of debris
-prob1 = ODEProblem(GravitationalForce,IC1,tspan,params)
-prob2 = ODEProblem(GravitationalForce,IC2,tspan,params)
-sol1 = DifferentialEquations.solve(prob1,saveat = satPOMDP.dt) # hope this is right ...
-sol2 = DifferentialEquations.solve(prob2,saveat = satPOMDP.dt)
-
-sp = [sol1.u[end]; sol2.u[end]]
-
-r = norm(sp[1:3]-sp[7:9])
-
-Rclose = satPOMDP.r_close*1/r^2
-Rburn = satPOMDP.r_burn*abs(0)   # if just tangential burns are possible, may redefine later
-
-R = Rclose + Rburn
-
-# OBSERVATION ...
-o = sp
-=#
+test_s,test_r,test_o = POMDPs.generate_sor(satPOMDP,s,0)
